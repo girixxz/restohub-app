ab3599666afbdd20d2ccf4133a373dbeb092004f
Aditya Giri <adityagiri206@gmail.com>
2025-07-06 23:56:58 +0700

    deploy fix

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ from datetime import timedelta
 
 class Config:
     SECRET_KEY = 'restohub-secret-key-2024'
-    MONGODB_URI = os.getenv('MONGODB_URI')
+    MONGODB_URI = os.getenv("MONGODB_URI")
     PERMANENT_SESSION_LIFETIME = timedelta(hours=2)
