bfb3cc8bb8579cdcb0d01f411c617ad6987f1585
Aditya Giri <adityagiri206@gmail.com>
2025-07-06 23:29:37 +0700

    fix structure for Railway

--- config.py
+++ config.py
@@ -1,14 +1,7 @@
 import os
 from datetime import timedelta
-from dotenv import load_dotenv
-
-# Load .env file
-load_dotenv()
 
 class Config:
     SECRET_KEY = 'restohub-secret-key-2024'
-    
-    # MongoDB URI diambil dari environment
     MONGODB_URI = os.getenv('MONGODB_URI')
-    
     PERMANENT_SESSION_LIFETIME = timedelta(hours=2)
